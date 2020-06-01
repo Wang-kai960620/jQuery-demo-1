d89f8c4e3b4f9e2a74a6b9b368fe1180e520e60b
wangkai <wk.dong.up@gmail.com>
2020-06-01 22:35:55 +0800

    init

---
+++ src/jquery.js
@@ -0,0 +1,42 @@
+window.jQuery = function (selectorOrArray) {
+  let elements;
+  if (typeof selectorOrArray === "string") {
+    elements = document.querySelectorAll(selectorOrArray);
+  } else if (selectorOrArray instanceof Array) {
+    elements = selectorOrArray;
+  }
+  // api 可以操作elements
+  return {
+    //闭包：函数访问外部变量
+    addClass(className) {
+      for (let i = 0; i < elements.length; i++) {
+        elements[i].classList.add(className);
+      }
+      return this;
+    },
+    each(fn) {
+      for (let i = 0; i < elements.length; i++) {
+        fn.call(null, elements[i], i);
+      }
+      return this;
+    },
+    find(selector) {
+      let array = [];
+      for (let i = 0; i < elements.length; i++) {
+        array = array.concat(
+          Array.from(elements[i].querySelectorAll(selector))
+        );
+      }
+      const newApi = jQuery(array);
+      return newApi;
+    },
+    parent() {
+      const array = [];
+      this.each((node) => {
+        if (array.indexOf(node.parentNode) === -1) {
+          array.push(node.parentNode);
+        }
+      });
+    },
+  };
+};
